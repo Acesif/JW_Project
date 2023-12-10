525b28c61dbeeb3ebe13f089311fac21205ff54f
Acesif <36653429+Acesif@users.noreply.github.com>
2023-12-10 15:30:43 +0600

    managed to import json file

--- visualize/views.py
+++ visualize/views.py
@@ -1,5 +1,13 @@
 from django.shortcuts import render
+import json
+import os
+from django.conf import settings
 
-# Create your views here.
+file_path = os.path.join(settings.BASE_DIR, 'visualize/stock_market_data.json')
+
 def home(request):
-    return render(request, "base.html")
+    with open(file_path) as f:
+        json_data = json.load(f)
+
+    return render(request, 'base.html', {'json_data': json_data})
+

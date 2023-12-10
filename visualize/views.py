60c4170c59e826c3b9040d771fad2763e002dd89
Acesif <36653429+Acesif@users.noreply.github.com>
2023-12-10 20:05:22 +0600

    json data in table

--- visualize/views.py
+++ visualize/views.py
@@ -8,6 +8,5 @@ file_path = os.path.join(settings.BASE_DIR, 'visualize/stock_market_data.json')
 def home(request):
     with open(file_path) as f:
         json_data = json.load(f)
-
     return render(request, 'base.html', {'json_data': json_data})
 

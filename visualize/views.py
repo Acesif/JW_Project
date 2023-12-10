af4755190223fc5f3eac9b8b5df8ca8cb672bb9b
Acesif <36653429+Acesif@users.noreply.github.com>
2023-12-10 20:19:49 +0600

    sql Model

--- visualize/views.py
+++ visualize/views.py
@@ -1,12 +1,5 @@
 from django.shortcuts import render
-import json
-import os
-from django.conf import settings
-
-file_path = os.path.join(settings.BASE_DIR, 'visualize/stock_market_data.json')
 
 def home(request):
-    with open(file_path) as f:
-        json_data = json.load(f)
-    return render(request, 'base.html', {'json_data': json_data})
+    return render(request, 'base.html')
 

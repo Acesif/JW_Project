36ba76077cea0d0d47917a478135193973cc976c
Acesif <36653429+Acesif@users.noreply.github.com>
2023-12-11 05:34:51 +0600

    failed attempts at line chart

--- visualize/views.py
+++ visualize/views.py
@@ -1,7 +1,17 @@
 from django.shortcuts import render
 from .models import stockModel
+import plotly.express as px
 
 def home(request):
     items = stockModel.objects.all()
     return render(request, 'base.html', {"items": items})
+
+def chart(request):
+    items = stockModel.objects.all() 
+    fig = px.line(
+        x=[i.date for i in items],
+        y=[i.close for i in items],
+    )
+    chart = fig.to_html()
+    return render(request, 'chart.html', {'chart': chart})
 

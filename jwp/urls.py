029a9e06800ca8f2ade8d3095b5edf88597ad77d
Acesif <36653429+Acesif@users.noreply.github.com>
2023-12-14 00:38:34 +0600

    added staticfiles

--- jwp/urls.py
+++ jwp/urls.py
@@ -15,9 +15,11 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
+from django.contrib.staticfiles.url import staticfiles_urlpatterns
 from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path("", include("visualize.urls"))
 ]
+urlpatterns += staticfiles_urlpatterns()
